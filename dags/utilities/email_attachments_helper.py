72d80e6698ef544af3b2b0a2bd409ec9449162e0
ConBle <conor.bleakley@softwire.com>
2025-05-02 11:48:05 +0100

    158 feat: update how powerpoints are written

--- dags/utilities/email_attachments_helper.py
+++ dags/utilities/email_attachments_helper.py
@@ -11,6 +11,7 @@ import pptx
 
 logger = logging.getLogger(__name__)
 
+
 def _extract_text_pdf(pdf_file: io.BytesIO) -> str:
     doc = pymupdf.open(stream=pdf_file, filetype="pdf")
 
@@ -23,6 +24,7 @@ def _extract_text_pdf(pdf_file: io.BytesIO) -> str:
 
     return text
 
+
 def _extract_text_excel(excel_file: io.BytesIO) -> str:
     all_sheets = pd.read_excel(excel_file, sheet_name=None)
 
@@ -31,23 +33,28 @@ def _extract_text_excel(excel_file: io.BytesIO) -> str:
         csv_buffer = io.StringIO()
         df.to_csv(csv_buffer, index=False)
         csv_outputs[sheet_name] = csv_buffer.getvalue()
-    
+
     return json.dumps(csv_outputs)
+
 
 def _extract_text_powerpoint(powerpoint_file: io.BytesIO) -> str:
     presentation = pptx.Presentation(powerpoint_file)
 
-    text = ""
+    all_slides = []
     for i, slide in enumerate(presentation.slides):
+        shapes_on_slide = []
         for shape in slide.shapes:
-            text += f"slide: {i}, text: {shape.text}, "
+            shapes_on_slide.append({"shape_text": shape.text})
+        all_slides.append({"slide_number": i + 1, "shapes": shapes_on_slide})
+
+    return json.dumps({"presentation": all_slides})
 
 
 SUPPORTED_FILE_TYPES: dict[str, Callable[[io.BytesIO], str]] = {
     "application/vnd.openxmlformats-officedocument.wordprocessingml.document": docx2txt.process,
     "application/pdf": _extract_text_pdf,
     "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": _extract_text_excel,
-    "application/vnd.openxmlformats-officedocument.presentationml.presentation": _extract_text_powerpoint
+    "application/vnd.openxmlformats-officedocument.presentationml.presentation": _extract_text_powerpoint,
 }
 
 
@@ -86,7 +93,7 @@ def _parse_attachment(msgraph_attachment: dict) -> Optional[dict]:
     attachment_text = SUPPORTED_FILE_TYPES[file_type](attachment_file)
 
     return {
-        "original_content_type": file_type,
+        "original_file_type": file_type,
         "file_name": msgraph_attachment["name"],
         "text": attachment_text,
     }
