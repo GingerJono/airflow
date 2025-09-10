1eb252cf9e2cb71f6e97acb612e202cdfdac24b3
Claire Li <Claire.Li@softwire.com>
2025-09-10 10:00:25 +0100

    393: refactor

--- plugins/helpers/cytora_helper.py
+++ plugins/helpers/cytora_helper.py
@@ -24,6 +24,7 @@ DATETIME_FORMATS = [
 
 
 def get_field_value(output: dict, key: str):
+    """Safely extract output["fields"][key]["value"], or return None."""
     try:
         node = output.get("fields", {}).get(key, {})
         return node.get("value")
@@ -31,13 +32,16 @@ def get_field_value(output: dict, key: str):
         return None
 
 
-def to_str_or_none(v):
+def safe_stringify(v):
+    """Return str(value) if value is not None, else None."""
     return None if v is None else str(v)
 
 
-def parse_date_or_none(v):
+def safe_parse_date(v):
+    """Try to parse a string using known datetime formats; return date or None."""
     if not v:
         return None
+
     s = str(v).strip()
     from datetime import datetime as dt
 
@@ -50,24 +54,24 @@ def parse_date_or_none(v):
 
 
 CYTORA_OUTPUT_FIELD_MAP_MAIN = {
-    "OutputInsuredName": ("insured_name", to_str_or_none),
-    "OutputInsuredDomicile": ("insured_domicile", to_str_or_none),
-    "OutputInsuredState": ("insured_state", to_str_or_none),
-    "OutputReinsuredName": ("reinsured_name", to_str_or_none),
-    "OutputReinsuredDomicile": ("reinsured_domicile", to_str_or_none),
-    "OutputReinsuredState": ("reinsured_state", to_str_or_none),
-    "OutputBrokerCompany": ("broker_company", to_str_or_none),
-    "OutputBrokerContact": ("broker_contact", to_str_or_none),
-    "OutputLineOfBusiness": ("line_of_business", to_str_or_none),
-    "OutputPolicyType": ("policy_type", to_str_or_none),
-    "OutputUnderwriter": ("underwriter", to_str_or_none),
-    "OutputInceptionDate": ("inception_date", parse_date_or_none),
-    "OutputExpiryDate": ("expiry_date", parse_date_or_none),
-    "OutputRiskLocation": ("risk_location", to_str_or_none),
-    "OutputAdditionalParties": ("additional_parties", to_str_or_none),
-    "OutputSubmissionSummary": ("submission_summary", to_str_or_none),
-    "OutputNewRenewal": ("new_vs_renewal", to_str_or_none),
-    "OutputRenewedFrom": ("renewed_from", to_str_or_none),
+    "OutputInsuredName": ("insured_name", safe_stringify),
+    "OutputInsuredDomicile": ("insured_domicile", safe_stringify),
+    "OutputInsuredState": ("insured_state", safe_stringify),
+    "OutputReinsuredName": ("reinsured_name", safe_stringify),
+    "OutputReinsuredDomicile": ("reinsured_domicile", safe_stringify),
+    "OutputReinsuredState": ("reinsured_state", safe_stringify),
+    "OutputBrokerCompany": ("broker_company", safe_stringify),
+    "OutputBrokerContact": ("broker_contact", safe_stringify),
+    "OutputLineOfBusiness": ("line_of_business", safe_stringify),
+    "OutputPolicyType": ("policy_type", safe_stringify),
+    "OutputUnderwriter": ("underwriter", safe_stringify),
+    "OutputInceptionDate": ("inception_date", safe_parse_date),
+    "OutputExpiryDate": ("expiry_date", safe_parse_date),
+    "OutputRiskLocation": ("risk_location", safe_stringify),
+    "OutputAdditionalParties": ("additional_parties", safe_stringify),
+    "OutputSubmissionSummary": ("submission_summary", safe_stringify),
+    "OutputNewRenewal": ("new_vs_renewal", safe_stringify),
+    "OutputRenewedFrom": ("renewed_from", safe_stringify),
 }
 
 
